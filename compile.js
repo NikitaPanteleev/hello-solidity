aa0a4ccfb297cb8612bfbb93c28ad00a5629f482
Nikita Panteleev <nikita.panteleev@phystech.edu>
2023-01-22 12:01:37 +0000

    Add lottery contract

--- compile.js
+++ compile.js
@@ -4,25 +4,25 @@ const solc = require('solc');
 
 const buildPath = path.resolve(__dirname, 'build');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const inboxPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
 const source = fs.readFileSync(inboxPath, 'UTF-8');
 
 var input = {
     language: 'Solidity',
     sources: {
-        'Inbox.sol' : {
+        'Lottery.sol': {
             content: source
         }
     },
     settings: {
         outputSelection: {
             '*': {
-                '*': [ '*' ]
+                '*': ['*']
             }
         }
     }
 };
 
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
-module.exports = output.contracts['Inbox.sol']['Inbox'];
-// console.log(Object.keys(module.exports));
+module.exports = output.contracts['Lottery.sol']['Lottery'];
+console.log(Object.keys(module.exports));
--- deploy.js
+++ deploy.js
@@ -4,7 +4,7 @@ const Web3 = require('web3');
 const { abi, evm } = require('./compile');
 
 const provider = new HDWalletProvider(
-    'toward cannon couple struggle magnet spy tooth prosper salad omit trick purpose',
+    '',
     'https://goerli.infura.io/v3/1e9e716253134c85924216c52f9f8070'
 )
 const web3 = new Web3(provider);
@@ -14,7 +14,7 @@ const deploy = async () => {
     console.log('Attempting to get from', accounts[0]);
 
     result = await new web3.eth.Contract(abi)
-        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+        .deploy({ data: evm.bytecode.object })
         .send({ from: accounts[0], gas: '1000000' });
 
     provider.engine.stop();
